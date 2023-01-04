ffd38b82989ea0709f33c4ccbbf9de9819b5a714
Bearei <bear_ei@outlook.com>
2023-01-04 09:37:10 +0800

    feat: version 0.0.10

--- test/components/Modal.test.tsx
+++ test/components/Modal.test.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import Modal from '../../src/components/Modal';
-import { render } from '../utils/test_utils';
+import { render } from '../utils/test-utils';
 
 describe('test/components/Modal.test.ts', () => {
   test('It should be a render modal', async () => {
